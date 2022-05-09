06bba18d3dd29f2bc4db27806be40416aad2a3a1
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2022-05-09 13:56:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,9 +34,9 @@ function draw() {
     if (mouseIsPressed) {
         line(pmouseX, pmouseY, mouseX, mouseY);
     }
-    check_sketch()
-    if (drawn_sketch == sketch) {
-        answer_holder = "set"
+    check_sketch() ;{
+        if (drawn_sketch == sketch)
+            answer_holder = "set"
         score++;
         document.getElementById('score').innerHTML = 'Score: ' + score;
     }
@@ -45,3 +45,27 @@ function draw() {
 function classifyCanvas() {
     classifier.classify(canvas, gotResult);
 }
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+}
+
+function check_sketch() {
+    timer_counter++;
+    document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
+    console.log(timer_counter) 
+    if (timer_counter > 400) {
+        timer_counter = 0;
+        timer_check = "completed"
+    }
+    if (timer_check == "completed" || answer_holder == "set") {
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }}
